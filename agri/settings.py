f7913cd5fb864a4f32d09c225895fd2d80f2bd39
samuelmutwiri <92987039+samuelmutwiri@users.noreply.github.com>
2024-03-08 17:20:09 +0000

    Created acc app to Handle user accounts Created Super user

--- agri/settings.py
+++ agri/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'agri.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["agri/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
